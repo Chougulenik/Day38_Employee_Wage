b8870abcee40391530e457297c30dcb8f377649e
Chougulenik <nikitachougule47@gmail.com>
2022-04-22 13:19:44 -0400

    added

--- EmployeeWage/employeewage.js
+++ EmployeeWage/employeewage.js
@@ -3,6 +3,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS =8
 const WAGE_PER_HOURS = 20
+const NUM_OF_WORKING_DAYS = 2
 
 function getWorkingHours(empCheck) {
     switch(empCheck) {
@@ -14,8 +15,13 @@ function getWorkingHours(empCheck) {
             return 0        
     }
 }
-let empCheck = Math.floor(Math.random() *10) %3
-let empHrs = getWorkingHours(empCheck)
+let empHrs = 0
+for(let day = 0; day <NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() *10) %3
+    empHrs += getWorkingHours(empCheck)
+
+}
 let empWage = empHrs * WAGE_PER_HOURS
-console.log("Hours : "+ empHrs +" Emp Wage : "+empWage)
+console.log("total Hrs :" + empHrs + " Emp Wage :"+empWage)
+
 
